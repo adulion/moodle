b0da7591116ab56f075584927df3811b3939ab48
Chris McCabe <chris@justni.com>
2019-05-02 23:29:50 +0100

    Report to show completetion of users

---
+++ details.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Run the code checker from the web.
+ *
+ * @package    local_coursecompletionstats
+ * @copyright  2019 Chris McCabe
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+
+$userid = optional_param('user_id', 0, PARAM_INT);
+
+admin_externalpage_setup('coursecompletionstats', '', null, '', array('pagelayout' => 'report'));
+
+$usercourses = $DB->get_records_sql('SELECT ue.id, ue.userid, e.courseid, c.fullname, cc.id completion_id, cc.timecompleted FROM
+{user_enrolments} ue JOIN {enrol} e ON ( ue.enrolid = e.id) JOIN {course} c ON e.courseid = c.idAND userid = :id LEFT JOIN
+{course_completions} cc ON cc.userid = ue.userid AND cc.course = e.courseid ORDER BY c.fullname ASC', ['id' => $userid]);
+
+echo $OUTPUT->header();
+
+$table = new html_table();
+
+$table->head = array(get_string('lb_course_name', 'local_coursecompletionstatus'),
+        get_string('lb_completion_status', 'local_coursecompletionstatus'),
+        get_string('lb_completed_on', 'local_coursecompletionstatus'));
+
+foreach ($usercourses as $course) {
+    $row = array();
+
+    $row[] = html_writer::link(new moodle_url('/course/view.php', array('id' => $course->courseid)), $course->fullname);
+    $row[] = is_null($course->completion_id) ? 'Incomplete' : 'Completed';
+    $row[] = is_null($course->timecompleted) ? 'Incomplete' :
+            userdate($course->timecompleted, get_string('strftimedatetime', 'langconfig'));
+
+    $table->data[] = $row;
+}
+echo html_writer::table($table);
+
+echo $OUTPUT->footer();
+
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Run the code checker from the web.
+ *
+ * @package    local_coursecompletionstats
+ * @copyright  2019 Chris McCabe
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+require(dirname(__FILE__) . '/../../config.php');
+require_once($CFG->libdir . '/adminlib.php');
+
+admin_externalpage_setup('coursecompletionstats', '', null, '', array('pagelayout' => 'report'));
+
+echo $OUTPUT->header();
+
+$table = new html_table();
+
+$table->head = array(get_string('lb_first_name', 'lcoursecompletionstats'),
+        get_string('lb_last_name', 'local_coursecompletionstatus'),
+        '',
+);
+
+foreach (get_users() as $user) {
+
+    $row = array();
+
+    $row[] = $user->firstname;
+    $row[] = $user->lastname;
+    $row[] = html_writer::link(
+            new moodle_url($CFG->wwwroot . '/local/coursecompletionstatus/details.php?user_id=' . $user->id), 'Visit'
+    );
+
+    $table->data[] = $row;
+}
+
+echo html_writer::table($table);
+
+echo $OUTPUT->footer();
